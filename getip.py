c55f9cbf561ec95e276e94622cd3a9d405bcec32
Hook <war7ng@qq.com>
2023-03-06 16:38:36 +0800

    123123

---
+++ getip.py
@@ -0,0 +1,11 @@
+from flask import Flask,request
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return request.remote_addr
+
+if __name__ =="__main__":
+    app.run(port=8080,host="::")

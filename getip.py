d6b1a23e9f84edb66bd23d18b5aa738f30b5898d
yangchao <war7ng@qq.com>
2023-03-06 20:20:26 -0500

    123123

--- getip.py
+++ getip.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return request.remote_addr
+    return request.headers.get('X-Real-IP', request.remote_addr)
 
 if __name__ =="__main__":
     app.run(port=8080,host="::")

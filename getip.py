03c016ed9c79e4a6e118251b9ffff12815dd000b
yangchao <war7ng@qq.com>
2023-03-11 13:37:41 -0500

    asdf

--- getip.py
+++ getip.py
@@ -1,6 +1,7 @@
 from flask import Flask,request
 import pymongo
 from datetime import datetime
+import ipaddress
 
 client = pymongo.MongoClient("mongodb+srv://by9559:2FjNWHfZNnoHcoIY@serverdb.tgnra.mongodb.net/?retryWrites=true&w=majority")
 db = client.get_database("test")
@@ -10,9 +11,10 @@ c = db.get_collection("DDNS")
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def main():
-    ip = request.headers.get('X-Real-IP', request.remote_addr)
+    ip = request.headers.get('X-Forwarded-For', request.remote_addr).replace(" ","").split(",")[0]
     if not [i for i in c.find({"domain":ip})]:
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         c.insert_one({"domain":ip, 'date': current_time})

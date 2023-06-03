7c6abcc5e6e2fb54594b77f0e0a97d79ed77399d
杨超 <98072436+hook-7@users.noreply.github.com>
2023-06-03 15:49:22 +0000

    123123

--- start.sh
+++ start.sh
@@ -1,2 +1,17 @@
-pip install flask pymongo  
-python /getip.py
+# apt-get update && apt-get install -y curl lsb-release gnupg ca-certificates nginx
+
+# curl https://pkg.cloudflareclient.com/pubkey.gpg | gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
+
+# echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/cloudflare-client.list
+
+# apt-get update
+
+# apt-get install -y cloudflare-warp
+
+nohup warp-svc &
+
+warp-cli register 
+
+warp-cli set-mode proxy
+
+warp-cli connect

9772b8d7a19163e44ce55e29ad8bb6e15ea63126
杨超 <98072436+hook-7@users.noreply.github.com>
2023-06-03 16:03:32 +0000

    312312

--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
-# apt-get update && apt-get install -y curl lsb-release gnupg ca-certificates nginx
+apt-get update && apt-get install -y curl lsb-release gnupg ca-certificates nginx
 
-# curl https://pkg.cloudflareclient.com/pubkey.gpg | gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
+curl https://pkg.cloudflareclient.com/pubkey.gpg | gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
 
-# echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/cloudflare-client.list
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/cloudflare-client.list
 
-# apt-get update
+apt-get update
 
-# apt-get install -y cloudflare-warp
+apt-get install -y cloudflare-warp
 
-nohup warp-svc &
+# nohup warp-svc &
+sleep 10
 
 warp-cli register 
 
